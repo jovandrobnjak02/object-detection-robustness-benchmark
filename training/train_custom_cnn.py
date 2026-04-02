0fe51ec6994a6e6adf35b69a3d43712a9fc206ed
Jovan Drobnjak <drobnjakj02@gmail.com>
2026-04-02 18:06:48 +0200

    batch size

--- training/train_custom_cnn.py
+++ training/train_custom_cnn.py
@@ -15,7 +15,7 @@ from evaluation.metrics import compute_map, box_cxcywh_to_xyxy
 
 EPOCHS         = 100
 WARMUP_EPOCHS  = 3
-BATCH_SIZE     = 32
+BATCH_SIZE     = 64
 NUM_WORKERS    = 4
 IMG_SIZE       = 640
 LR             = 1e-3
